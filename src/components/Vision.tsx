f636c038f59dc907b711b3a943630165b56a788e
Anburaj Nadar <Anburaj@la86.in>
2025-08-11 02:47:00 -0500

    Updated Vision.tsx

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -17,7 +17,7 @@ const Vision: React.FC = () => {
     <img
       src="/assets/images/mitech-landing-case-study-preview.jpg"
       alt="Mission and Vision"
-      className="max-w-full lg:max-w-xl xl:max-w-2xl h-auto object-contain"
+      className="max-w-full w-full h-[400px] lg:h-[500px] xl:h-[600px] object-cover"
     />
   </div>
         <div className="w-full lg:w-1/2 flex items-center justify-center">
