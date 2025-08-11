c97937fa1de69e7515570d32cafc2520835faa51
Anburaj Nadar <Anburaj@la86.in>
2025-08-11 01:46:04 -0500

    Updated Vision.tsx

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -9,7 +9,7 @@ const Vision: React.FC = () => {
       <div className="w-full flex flex-col lg:flex-row gap-8 md:gap-12 items-center h-full">
         <div className="w-full lg:w-1/2 h-64 sm:h-80 md:h-96 lg:h-full flex items-center justify-center overflow-hidden rounded-lg">
           <img
-            src="/assets/images/project-mission-vission-illustration_593183-510.webp" 
+            src="/assets/images/ideas-progress-vision-inspiration-design-concept_53876-123704.jpg" 
             alt="Mission and Vision"
             className="w-full h-full object-contain"
           />
