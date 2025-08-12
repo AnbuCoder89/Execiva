f5b30d956bc2285ebac9f4ab250d950eca9759da
Anburaj Nadar <Anburaj@la86.in>
2025-08-12 02:07:16 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import CaseStudies from './components/CaseStudies';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import AnimatedButtonDemo from './components/AnimatedButtonDemo';
 
 function App() {
   useEffect(() => {
@@ -33,7 +32,6 @@ function App() {
         <Testimonials />
         <Contact />
         <Footer />
-        <AnimatedButtonDemo />
       </div>
     </ParallaxProvider>
   );
