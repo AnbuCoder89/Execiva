50a77242f5593a477a4cf2b682e8209613e478dc
Anburaj Nadar <Anburaj@la86.in>
2025-08-12 04:36:18 -0500

    Updated CaseStudies.tsx

--- src/components/CaseStudies.tsx
+++ src/components/CaseStudies.tsx
@@ -40,7 +40,7 @@ const CaseStudies: React.FC = () => {
         {/* Image Column */}
         <div className="w-full lg:w-1/2 flex items-center justify-center p-4 sm:p-6 lg:p-10">
           <img
-            src="/assets/images/data_analytics/data-dashboard-2.jpeg"
+            src="/assets/images/case-studies/case-1.jpeg"
             alt="Case Studies"
             className="w-[85%] h-auto object-contain"
           />
