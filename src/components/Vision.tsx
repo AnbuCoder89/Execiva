ebcaf6da4a1ee71f7070e6e194052edc163c29b9
Anburaj Nadar <Anburaj@la86.in>
2025-08-12 02:38:53 -0500

    Updated Vision.tsx

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -15,7 +15,7 @@ const Vision: React.FC = () => {
     >
 <div className="w-full flex flex-col lg:flex-row items-center justify-center">
   {/* Image Column */}
-  <div className="w-full lg:w-1/2 flex items-center justify-center">
+  <div className="w-full lg:w-1/2 flex items-center justify-center p-4 sm:p-6 lg:p-10">
     <img
       src="/assets/images/vision3.jpeg"
       alt="Mission and Vision"
