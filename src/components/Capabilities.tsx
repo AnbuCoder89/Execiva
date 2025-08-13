f7e4e82349b068f27ed288e974a5654945846829
Anburaj Nadar <Anburaj@la86.in>
2025-08-13 00:36:10 -0500

    Updated Capabilities.tsx

--- src/components/Capabilities.tsx
+++ src/components/Capabilities.tsx
@@ -120,9 +120,9 @@ const Capabilities: React.FC = () => {
                     <div className="p-6 md:p-8">
                       <h3 className="text-2xl md:text-3xl lg:text-4xl font-light text-white leading-tight font-sf-pro-display">
                         {capability.title.split(' ')[0]}
-                        <span className="block font-bold mt-1">
+                        
                           {capability.title.split(' ').slice(1).join(' ')}
-                        </span>
+                        
                       </h3>
                     </div>
                   </div>
