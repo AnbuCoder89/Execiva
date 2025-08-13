6a362b16db751baaadb086fcd8e32b662951d704
Anburaj Nadar <Anburaj@la86.in>
2025-08-12 22:57:55 -0500

    Add TCSServices component to App

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Hero from './components/Hero';
 import Vision from './components/Vision';
 import Capabilities from './components/Capabilities';
 import Services from './components/Services';
+import TCSServices from './components/TCSServices';
 import CaseStudies from './components/CaseStudies';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
@@ -25,6 +26,7 @@ function App() {
         <Hero />
         <Vision />
         <Services />
+        <TCSServices />
         <Capabilities />
         <CaseStudies />
         <Testimonials />
