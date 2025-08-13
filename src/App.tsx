e13b5bf2f6166340b050e43b5812c3f4af85a727
Anburaj Nadar <Anburaj@la86.in>
2025-08-12 22:49:25 -0500

    Fix missing Capabilities2 import

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import 'aos/dist/aos.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Vision from './components/Vision';
+import Capabilities2 from './components/Capabilities2';
 import Capabilities from './components/Capabilities';
 import Services from './components/Services';
 import CaseStudies from './components/CaseStudies';
