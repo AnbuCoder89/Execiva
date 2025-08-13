885b3e81a354b4ac48c11deca43100b96037119f
Anburaj Nadar <Anburaj@la86.in>
2025-08-13 02:06:19 -0500

    Fix JSX syntax error in Capabilities component

--- src/components/Capabilities.tsx
+++ src/components/Capabilities.tsx
@@ -116,8 +116,7 @@ const Capabilities: React.FC = () => {
                   <div
                     className="absolute inset-0 bg-cover bg-center rounded-xl pointer-events-none"
                     style={{ backgroundImage: `url('${capability.image}')` }}
-                  />
-                    <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-black/60 to-transparent h-1/2 rounded-t-xl" />
+                  >
                   </div>
 
                   {/* Title */}
