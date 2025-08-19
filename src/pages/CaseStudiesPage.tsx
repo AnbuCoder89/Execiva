0a41c1f43db4b8567e48179839ccec7c8f755ded
Anburaj Nadar <Anburaj@la86.in>
2025-08-19 02:03:15 -0500

    Fix header overlap on Case Studies page

--- src/pages/CaseStudiesPage.tsx
+++ src/pages/CaseStudiesPage.tsx
@@ -187,7 +187,7 @@ const CaseStudiesPage: React.FC = () => {
   );
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 pt-20">
       {/* Header */}
       <div className="bg-white shadow-sm border-b border-gray-200">
         <div className="px-6 py-6">
