5b566c463718c3f8b4748e6f90eda27afdd3d371
Anburaj Nadar <Anburaj@la86.in>
2025-08-20 03:06:06 -0500

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -94,8 +94,8 @@ const Header = () => {
   className={`fixed w-full z-50 transition-all duration-500 ${
     isScrolled
       ? "bg-white/80 shadow-sm py-2"
-      : "bg-white/80 shadow-sm py-2"
-  }`}
+      : "bg-transparent"
+  } py-2`}
 >
 
       <nav className="max-w-7xl mx-auto px-6">
