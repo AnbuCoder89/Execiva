85deea0ee8beb902552f157a461a6bf5b0922496
Anburaj Nadar <Anburaj@la86.in>
2025-08-20 02:02:35 -0500

    Updated Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -61,7 +61,7 @@ const Services: React.FC = () => {
       ref={sectionRef}
       className="relative w-full min-h-screen flex items-center justify-center bg-gray-50 p-4 sm:p-6 lg:p-8"
       >
-      <div className="w-full max-w-7xl mx-auto">
+      <div className="w-full">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 sm:gap-6 lg:gap-8 min-h-[80vh] lg:min-h-[85vh]">
           {services.map((service, index) => (
             <div
