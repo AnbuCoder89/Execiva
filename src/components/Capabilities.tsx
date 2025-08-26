e51234a2f089c607ca15bf510396a05f10c1a3bd
Anburaj Nadar <Anburaj@la86.in>
2025-08-26 00:58:13 -0500

    Updated Capabilities.tsx

--- src/components/Capabilities.tsx
+++ src/components/Capabilities.tsx
@@ -5,6 +5,13 @@ import Button from "./ui/Button";
 import { ChevronRight, ChevronLeft } from "lucide-react";
 import "swiper/css";
 import "swiper/css/navigation";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Avatar,
+} from "@material-tailwind/react";
 
 const Capabilities: React.FC = () => {
   const sectionRef = useRef<HTMLDivElement>(null);
@@ -108,6 +115,40 @@ const capabilities = [
           From AI and Data to SEO, we transform complexity into clarity, building solutions that not only solve today's challenges but also create tomorrow's opportunities.
         </p>
       </div>
+
+      <Card
+      shadow={false}
+      className="relative grid h-[40rem] w-full max-w-[28rem] items-end justify-center overflow-hidden text-center"
+    >
+      <CardHeader
+        floated={false}
+        shadow={false}
+        color="transparent"
+        className="absolute inset-0 m-0 h-full w-full rounded-none bg-[url('https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')] bg-cover bg-center"
+      >
+        <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
+      </CardHeader>
+      <CardBody className="relative py-14 px-6 md:px-12">
+        <Typography
+          variant="h2"
+          color="white"
+          className="mb-6 font-medium leading-[1.5]"
+        >
+          How we design and code open-source projects?
+        </Typography>
+        <Typography variant="h5" className="mb-4 text-gray-400">
+          Tania Andrew
+        </Typography>
+        <Avatar
+          size="xl"
+          variant="circular"
+          alt="tania andrew"
+          className="border-2 border-white"
+          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+        />
+      </CardBody>
+    </Card>
+
 
       <div className="text-white w-full h-[60vh] rounded-xl flex items-center justify-center py-6 sm:py-8 md:py-10 relative">
         {/* Custom Arrows */}
