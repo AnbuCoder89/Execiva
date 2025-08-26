9c77d51040e2c575c04c49ed2caeab503904fbb7
Anburaj Nadar <Anburaj@la86.in>
2025-08-26 02:09:22 -0500

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -69,7 +69,7 @@ const Hero = () => {
               <div className="text-gray-400 font-bold text-lg md:text-xl font-sf-pro-display">
                 Consulting & Strategy
               </div>
-              <div className="text-black-400 font-bold text-lg md:text-xl font-sf-pro-display">
+              <div className="text-black-300 font-bold text-lg md:text-xl font-sf-pro-display">
                 AI & Data Insights
               </div>
             </div>
