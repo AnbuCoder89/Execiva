d9c267331640c6f111683781e3c568fa4c6acfc2
Anburaj Nadar <Anburaj@la86.in>
2025-08-26 01:17:57 -0500

    Make Card Heights Responsive

--- src/components/Capabilities.tsx
+++ src/components/Capabilities.tsx
@@ -73,6 +73,7 @@ const Capabilities: React.FC = () => {
               key={capability.title}
               className={`group relative w-full h-[400px] overflow-hidden rounded-xl 
                 shadow-xl hover:shadow-2xl transition-all duration-500 transform hover:scale-[1.02] hover:-translate-y-2
+                h-[280px] sm:h-[320px] md:h-[350px] lg:h-[400px] xl:h-[450px]
                 ${isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
               style={{ transitionDelay: `${index * 100}ms` }}
             >
