1515e2ede72a36569a6d66bfeb79b4a2ffc133b8
Anburaj Nadar <Anburaj@la86.in>
2025-08-28 01:59:29 -0500

    Updated CaseStudies.tsx

--- src/components/CaseStudies.tsx
+++ src/components/CaseStudies.tsx
@@ -25,6 +25,7 @@ const CaseStudies: React.FC = () => {
       observer.observe(sectionRef.current);
     }
 
+    
     return () => {
       observer.disconnect();
     };
@@ -67,7 +68,7 @@ const CaseStudies: React.FC = () => {
             <img
               src="/image/case-studies/case-1.jpeg"
               alt="Case Studies"
-              className="w-full max-w-md h-auto object-contain rounded-2xl shadow-lg"
+              className="w-full h-auto max-w-md rounded-2xl shadow-lg object-contain"
             />
           </div>
         </div>
