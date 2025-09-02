a4dc081838dfa64004a70c65d8f5e9856dec3850
Anburaj Nadar <Anburaj@la86.in>
2025-09-02 09:22:33 -0500

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -85,7 +85,7 @@ const Hero = () => {
         </div>
 
         {/* Trusted By Section */}
-        <div className="w-full pt-16 sm:pt-20 md:pt-24">
+        <div className="w-full pt-10 sm:pt-20 md:pt-24">
           {/* Headline */}
           <p className="text-sm sm:text-base md:text-lg text-gray-500 font-sf-pro-text mb-8 sm:mb-10 md:mb-12 font-medium tracking-wide">
             Our Capabilities
