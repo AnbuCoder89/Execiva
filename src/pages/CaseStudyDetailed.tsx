4c728ad66ac87d237be1a877ea7102410d573269
Anburaj Nadar <Anburaj@la86.in>
2025-09-06 01:22:14 -0500

    Updated CaseStudyDetailed.tsx

--- src/pages/CaseStudyDetailed.tsx
+++ src/pages/CaseStudyDetailed.tsx
@@ -152,8 +152,7 @@ const CaseStudyDetailed: React.FC = () => {
                       </motion.h1>
                     </div>
 
-                    {/* Client Industry Logo/Badge */}
-                    {/* <motion.ul 
+                    <motion.ul 
                       className="flex flex-wrap gap-4 lg:gap-8"
                       variants={itemVariants}
                     >
@@ -164,8 +163,7 @@ const CaseStudyDetailed: React.FC = () => {
                           </span>
                         </div>
                       </li>
-                    </motion.ul> */}
-                    
+                    </motion.ul>
                   </div>
                 </div>
               </motion.div>
