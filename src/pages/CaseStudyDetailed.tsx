1b3766df97e50ecb8957fbbac68ad9ed023de762
Anburaj Nadar <Anburaj@la86.in>
2025-09-06 01:08:36 -0500

    Updated CaseStudyDetailed.tsx

--- src/pages/CaseStudyDetailed.tsx
+++ src/pages/CaseStudyDetailed.tsx
@@ -120,7 +120,7 @@ const CaseStudyDetailed: React.FC = () => {
       <motion.section 
         className="text-black pt-20"
         style={{
-          background: 'linear-gradient(135deg, #f2f2f2 0%, #ada996 100%)'
+          background: '#ada996'
         }}
         variants={itemVariants}
       >
