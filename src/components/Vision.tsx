d8c8ec8939f39cafbd7cb059763966f9b9286ddd
Anburaj Nadar <Anburaj@la86.in>
2025-09-07 23:47:01 -0500

    Updated Vision.tsx

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -150,18 +150,17 @@ const Vision: React.FC = () => {
                   <div className="flex justify-center items-center h-full">
                     <figure className="w-full h-full">
                  <img
-  className="
-    w-full 
-    h-auto            /* mobile & tablet: auto height */
-    md:h-[50%]        /* ≥768px (desktop): 80% height */
-    object-cover 
-    rounded-lg 
-    md:rounded-2xl
-  "
-  src={item.image}
-  alt={item.title}
-/>
-
+                    className="
+                      w-full 
+                      h-auto            
+                      md:h-[50%]       
+                      object-cover 
+                      rounded-lg 
+                      md:rounded-2xl
+                    "
+                    src={item.image}
+                    alt={item.title}
+                  />
                     </figure>
                   </div>
                 </motion.div>
