608f5a9202e7b7864aee88c539a8ca8c241f0d91
Anburaj Nadar <Anburaj@la86.in>
2025-09-08 23:13:43 -0500

    Updated ServiceDetailedPage.tsx

--- src/components/services/ServiceDetailedPage.tsx
+++ src/components/services/ServiceDetailedPage.tsx
@@ -290,6 +290,8 @@ const ServiceDetailedPage: React.FC = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
     >
+  
+      
       <ServiceHero
         title={service.title}
         category={service.category}
