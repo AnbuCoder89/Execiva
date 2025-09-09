f2baa5265747787a35d7622bb728bcd14c6b290d
Anburaj Nadar <Anburaj@la86.in>
2025-09-09 01:45:49 -0500

    Updated HeroSection.tsx

--- src/components/caseStudy/HeroSection.tsx
+++ src/components/caseStudy/HeroSection.tsx
@@ -93,7 +93,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({ caseStudy, onBackClick }) => 
                 variant="ghost"
                 size="lg"
                 onClick={onBackClick}
-                className="text-white 
+                className="text-gray-300 
   border border-white/30
   hover:text-gray-300 
   hover:bg-white/20"
