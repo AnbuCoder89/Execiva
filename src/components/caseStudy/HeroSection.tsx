78b0c6178d6148337e56a91f87eb5adf05035971
Anburaj Nadar <Anburaj@la86.in>
2025-09-09 02:17:34 -0500

    Updated HeroSection.tsx

--- src/components/caseStudy/HeroSection.tsx
+++ src/components/caseStudy/HeroSection.tsx
@@ -104,7 +104,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({ caseStudy, onBackClick }) => 
         
         {/* Project Details - Positioned at bottom */}
         <motion.div 
-          className="absolute bottom-20 left-8 sm:left-12 flex flex-wrap gap-4 lg:gap-8"
+          className="absolute bottom-20 sm:left-12 flex flex-wrap gap-4 lg:gap-8"
           variants={itemVariants}
         >
           {/* REGION */}
