7720d0f4633567c741ab4370b9de5f0034173e8a
Anburaj Nadar <Anburaj@la86.in>
2025-09-09 01:12:41 -0500

    Add scroll indicator to ServiceHero2

--- src/components/services/ServiceHero2.tsx
+++ src/components/services/ServiceHero2.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, ArrowDown } from "lucide-react";
 import Button from "../ui/Button";
 
 interface ServiceHero2Props {
@@ -54,6 +54,16 @@ const ServiceHero2: React.FC<ServiceHero2Props> = ({
             </div>
           </div>
         </div>
+
+        {/* Scroll Indicator */}
+        <motion.div
+          className="absolute bottom-6 left-1/2 -translate-x-1/2 animate-bounce"
+          variants={itemVariants}
+        >
+          <div className="w-10 h-10 flex items-center justify-center border border-white/60 rounded-full">
+            <ArrowDown className="w-4 h-4 text-white" />
+          </div>
+        </motion.div>
       </div>
     </section>
   );
