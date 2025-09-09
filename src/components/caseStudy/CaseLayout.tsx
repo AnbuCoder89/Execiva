484740a5ac080c3148100ca3d5573354cc32ff87
Anburaj Nadar <Anburaj@la86.in>
2025-09-09 10:22:24 -0500

    Updated CaseLayout.tsx

--- src/components/caseStudy/CaseLayout.tsx
+++ src/components/caseStudy/CaseLayout.tsx
@@ -8,7 +8,7 @@ interface CaseLayoutProps {
 const CaseLayout: React.FC<CaseLayoutProps> = ({ toc, content }) => {
   return (
     <div className="min-h-screen bg-gray-50 py-8">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
         {/* Mobile: Stacked layout */}
         <div className="lg:hidden space-y-6">
           <div className="w-full">
