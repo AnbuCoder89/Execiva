a64bc062ddeb0b6b47808b3ac1ea65e5926886ce
Anburaj Nadar <Anburaj@la86.in>
2025-09-09 00:19:42 -0500

    Updated ServiceHero2.tsx

--- src/components/services/ServiceHero2.tsx
+++ src/components/services/ServiceHero2.tsx
@@ -44,7 +44,7 @@ const ServiceHero2: React.FC<ServiceHero2Props> = ({
           <img 
             src="/image/vision/vision-1.jpeg" 
             alt="Service Hero" 
-            className="w-full h-screen object-cover rounded-2xl"
+            className="w-full h-screen object-cover rounded-2xl mb-8"
           />
         </div>
       </div>
