20ef1ec11d34c653734976debd3f14279c39bfa2
Anburaj Nadar <Anburaj@la86.in>
2025-09-08 23:55:45 -0500

    Updated ServiceHero2.tsx

--- src/components/services/ServiceHero2.tsx
+++ src/components/services/ServiceHero2.tsx
@@ -39,11 +39,11 @@ const ServiceHero2: React.FC<ServiceHero2Props> = ({
 
   return (
     <section className="min-h-screen flex items-center justify-center px-6 sm:px-8 lg:px-12 bg-gray-50">
-      <div className="bg-white rounded-2xl shadow-xl w-full p-8 sm:p-12 min-h-screen">
+      <div className="bg-white rounded-2xl shadow-xl w-full p-8 sm:p-12 ">
           <img 
             src="/image/vision/vision-2.jpeg" 
             alt="Service Hero" 
-            className="w-full object-cover rounded-2xl"
+            className="w-full object-cover rounded-2xl min-h-screen"
           />
         {/* add your content here later */}
       </div>
