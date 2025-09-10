8b53a3d289f27cd332efb288b4a49394afad80f3
Anburaj Nadar <Anburaj@la86.in>
2025-09-10 01:43:58 -0500

    Add image to div1 with rounded border and remove background

--- src/components/services/ServiceUseCases2.tsx
+++ src/components/services/ServiceUseCases2.tsx
@@ -9,12 +9,16 @@ const ServiceUseCases2: React.FC = () => {
           {/* Grid Container */}
           <div className="grid grid-cols-7 grid-rows-6 gap-2">
             {/* Div1 - spans 3 columns and 3 rows */}
-            <div className="col-span-3 row-span-3 bg-gray-100 rounded-lg flex items-center justify-center">
-              <span className="text-2xl font-bold text-gray-700">1</span>
+            <div className="col-span-3 row-span-3 rounded-lg overflow-hidden">
+              <img 
+                src="https://cdn.sanity.io/images/q9c9g16o/production/214d51304ca5e675e91994b7add6a7c370e5af15-8095x5399.jpg?w=1200&q=80"
+                alt="Service showcase"
+                className="w-full h-full object-cover"
+              />
             </div>
             
             {/* Div2 - spans 4 columns, 3 rows, starts at column 4 */}
-            <div className="col-span-4 row-span-3 col-start-4 bg-gray-200 rounded-lg flex items-center justify-center">
+            <div className="col-span-4 row-span-3 col-start-4 bg-gray-100 rounded-lg flex items-center justify-center">
               <span className="text-2xl font-bold text-gray-700">2</span>
             </div>
           </div>
