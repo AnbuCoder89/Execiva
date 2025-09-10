e12ed7fd4b5f494fba6879d12e0b0f0b73aa9659
Anburaj Nadar <Anburaj@la86.in>
2025-09-10 02:35:02 -0500

    Updated ServiceUseCases2.tsx

--- src/components/services/ServiceUseCases2.tsx
+++ src/components/services/ServiceUseCases2.tsx
@@ -19,6 +19,11 @@ const ServiceUseCases2: React.FC = () => {
             {/* Right Side - Content */}
             <div className="md:w-1/2 p-8 md:p-12 lg:p-8 flex flex-col justify-between h-[400px] md:h-[500px]">
               <div>
+                <span className="inline-block px-4 py-2 bg-beige text-gray-900 rounded-full text-sm font-medium font-sf-pro-text border border-gray-200">
+                  Website Redesign
+                </span>
+              </div>
+              <div>
               {/* Main Heading */}
               <p className="text-gray-900 text-xl md:text-2xl lg:text-[2.875rem] lg:leading-[3.25rem] font-medium leading-snug mb-6">
                 Get a dedicated Website Product Team to drive continuous growth.
@@ -31,11 +36,7 @@ const ServiceUseCases2: React.FC = () => {
               </div>
 
               {/* Author/Attribution Section */}
-              <div>
-                <span className="inline-block px-4 py-2 bg-beige text-gray-900 rounded-full text-sm font-medium font-sf-pro-text border border-gray-200">
-                  Website Redesign
-                </span>
-              </div>
+              
             </div>
           </div>
         </div>
