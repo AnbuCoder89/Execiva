18eea33147ec51b4f71d1561f2c935ae92f5f6da
Anburaj Nadar <Anburaj@la86.in>
2025-09-10 03:43:25 -0500

    Updated ServiceHero2.tsx

--- src/components/services/ServiceHero2.tsx
+++ src/components/services/ServiceHero2.tsx
@@ -49,7 +49,7 @@ const ServiceHero2: React.FC<ServiceHero2Props> = ({
                 Get Started
               </Button>
               <Button
-                variant=""
+                variant="ghost"
                 size="lg"
                 icon={ArrowLeft}
                 iconPosition="left"
