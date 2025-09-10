74b845ecc3af3f98c95375e10e6289138f3c5aae
Anburaj Nadar <Anburaj@la86.in>
2025-09-10 01:51:01 -0500

    Update grid to have two equal width divs

--- src/components/services/ServiceUseCases2.tsx
+++ src/components/services/ServiceUseCases2.tsx
@@ -13,12 +13,12 @@ const ServiceUseCases2: React.FC = () => {
               <img 
                 src="https://cdn.sanity.io/images/q9c9g16o/production/214d51304ca5e675e91994b7add6a7c370e5af15-8095x5399.jpg?w=1200&q=80"
                 alt="Service showcase"
-              />
-            </div>
-            
+        <div className="grid grid-cols-2 gap-2">
+          {/* Div1 - left side, spans 1 column */}
+          <div className="rounded-lg overflow-hidden">
             {/* Div2 - spans 4 columns, 4 rows, starts at column 4 */}
-            <div className="col-span-4 row-span-4 col-start-4 bg-gray-100 rounded-lg flex items-center justify-center">
-              <span className="text-gray-500 text-lg">2</span>
+          {/* Div2 - right side, spans 1 column */}
+          <div className="bg-gray-100 rounded-lg flex items-center justify-center">
             </div>
           </div>
         </div>
