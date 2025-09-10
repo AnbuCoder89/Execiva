a4189d3797ef4b369debb30951774919187519fe
Anburaj Nadar <Anburaj@la86.in>
2025-09-10 01:47:17 -0500

    Fix JSX syntax error in ServiceUseCases2

--- src/components/services/ServiceUseCases2.tsx
+++ src/components/services/ServiceUseCases2.tsx
@@ -13,13 +13,12 @@ const ServiceUseCases2: React.FC = () => {
               <img 
                 src="https://cdn.sanity.io/images/q9c9g16o/production/214d51304ca5e675e91994b7add6a7c370e5af15-8095x5399.jpg?w=1200&q=80"
                 alt="Service showcase"
-        <div className="grid grid-cols-7 grid-rows-4 gap-2">
               />
-          <div className="col-span-3 row-span-4 rounded-lg overflow-hidden">
+            </div>
             
-            {/* Div2 - spans 4 columns, 3 rows, starts at column 4 */}
-          {/* Div2 - spans 4 columns, 4 rows, starts at column 4 */}
-          <div className="col-span-4 row-span-4 col-start-4 bg-gray-100 rounded-lg flex items-center justify-center">
+            {/* Div2 - spans 4 columns, 4 rows, starts at column 4 */}
+            <div className="col-span-4 row-span-4 col-start-4 bg-gray-100 rounded-lg flex items-center justify-center">
+              <span className="text-gray-500 text-lg">2</span>
             </div>
           </div>
         </div>
