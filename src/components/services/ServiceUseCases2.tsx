4fdd925cc182d13bccc744ee555176fa44cd674a
Anburaj Nadar <Anburaj@la86.in>
2025-09-10 02:37:44 -0500

    Updated ServiceUseCases2.tsx

--- src/components/services/ServiceUseCases2.tsx
+++ src/components/services/ServiceUseCases2.tsx
@@ -17,7 +17,7 @@ const ServiceUseCases2: React.FC = () => {
             </div>
 
             {/* Right Side - Content */}
-            <div className="md:w-1/2 p-8 md:p-12 lg:p-8 flex flex-col justify-between">
+            <div className="md:w-1/2 p-8 md:p-12 lg:p-8 flex flex-col h-[400px] md:h-[500px]">
               <div>
                 <span className="inline-block px-4 py-2 bg-beige text-gray-900 rounded-full text-sm font-medium font-sf-pro-text border border-gray-200">
                   Website Redesign
