a84e1c194077a6aeb6102cfaa0f9a5eee92612aa
Anburaj Nadar <Anburaj@la86.in>
2025-09-10 09:21:22 -0500

    Updated WhyChooseUs.tsx

--- src/components/services/WhyChooseUs.tsx
+++ src/components/services/WhyChooseUs.tsx
@@ -85,7 +85,7 @@ const WhyChooseUs: React.FC<WhyChooseUsProps> = ({ serviceName = "our services" 
 
   return (
     <motion.section 
-      className="py-16 md:py-20 lg:py-24 bg-gray-50"
+      className="py-16 md:py-20 lg:py-24"
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true, margin: "-100px" }}
