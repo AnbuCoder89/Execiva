2753c525696d59e18b1eea65829c0b90e19e635f
Anburaj Nadar <Anburaj@la86.in>
2025-09-10 09:48:54 -0500

    Remove scroll indicator from ServiceHero2

--- src/components/services/ServiceHero2.tsx
+++ src/components/services/ServiceHero2.tsx
@@ -59,16 +59,6 @@ const ServiceHero2: React.FC<ServiceHero2Props> = ({
             </div>
           </div>
         </div>
-
-        {/* Scroll Indicator */}
-        <motion.div
-          className="absolute bottom-6 left-1/2 -translate-x-1/2 animate-bounce"
-          variants={itemVariants}
-        >
-          <div className="w-10 h-10 flex items-center justify-center border border-white/60 rounded-full">
-            <ArrowDown className="w-4 h-4 text-white" />
-          </div>
-        </motion.div>
       </div>
     </section>
   );
