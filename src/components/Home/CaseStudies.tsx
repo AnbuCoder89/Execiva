cbab2cfc0e1e57f8ef109b5373bc219ca1f4bbfc
Anburaj Nadar <Anburaj@la86.in>
2025-09-15 01:30:47 -0500

    Updated CaseStudies.tsx

--- src/components/Home/CaseStudies.tsx
+++ src/components/Home/CaseStudies.tsx
@@ -80,7 +80,7 @@ const CaseStudies: React.FC = () => {
     <section
       id="case-studies"
       ref={sectionRef}
-      className="relative w-full min-h-screen bg-white py-24 md:py-32 lg:py-40"
+      className="relative w-full min-h-screen bg-white"
     >
       <div className="mx-auto px-6 sm:px-8 lg:px-12">
         <motion.div 
