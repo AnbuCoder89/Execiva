01049b1592ea399812c39c9dfbae07a207ae80c4
Anburaj Nadar <Anburaj@la86.in>
2025-09-15 01:27:44 -0500

    Updated KeyMetrics.tsx

--- src/components/Home/KeyMetrics.tsx
+++ src/components/Home/KeyMetrics.tsx
@@ -55,7 +55,7 @@ const KeyMetrics: React.FC = () => {
     <>
       {/* Header Section */}
       <motion.section 
-        className="relative w-full min-h-screen flex flex-col justify-center bg-white"
+        className="relative w-full flex flex-col justify-center bg-white"
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true, margin: "-100px" }}
