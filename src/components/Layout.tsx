71068aa964ef58fac1333351a1511ca4215f140a
Anburaj Nadar <Anburaj@la86.in>
2025-09-16 02:01:32 -0500

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import NewHeader from './NewHeader'
 
 const Layout: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col scroll-smooth">
       <Header />
+      <NewHeader />
       <main className="flex-1">
         <Outlet />
       </main>
