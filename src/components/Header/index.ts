2e1c8a5fa7db9b40f5a773a16f97100b933bf626
Anburaj Nadar <Anburaj@la86.in>
2025-09-16 03:29:19 -0500

    Remove invalid export default type

--- src/components/Header/index.ts
+++ src/components/Header/index.ts
@@ -3,5 +3,3 @@ export { default as DesktopNav } from './DesktopNav';
 export { default as MobileNav } from './MobileNav';
 export { navItems } from './NavItems';
 export type { NavItem, SubMenuItem, MegaMenuCategory } from './NavItems';
-
-export default type
