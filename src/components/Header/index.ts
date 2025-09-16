2ccac7e4b365567c6ab02539ed1e7932f79fc4a1
Anburaj Nadar <Anburaj@la86.in>
2025-09-16 04:11:52 -0500

    Updated index.ts

--- src/components/Header/index.ts
+++ src/components/Header/index.ts
@@ -2,3 +2,6 @@ export { default as Logo } from './Logo';
 export { default as Navigation } from './Navigation';
 export { navItems } from './NavItems';
 export type { NavItem, SubMenuItem, MegaMenuCategory } from './NavItems';
+
+
+export default type
