183917f1af7403c76521e7a191f8f850105d14a6
Anburaj Nadar <Anburaj@la86.in>
2025-09-16 04:12:57 -0500

    Fix invalid export default type statement

--- src/components/Header/index.ts
+++ src/components/Header/index.ts
@@ -2,6 +2,3 @@ export { default as Logo } from './Logo';
 export { default as Navigation } from './Navigation';
 export { navItems } from './NavItems';
 export type { NavItem, SubMenuItem, MegaMenuCategory } from './NavItems';
-
-
-export default type
